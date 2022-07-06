9c51b5b813e1c8615782c5d8e3b1027e205e0728
Muhammed <bymadmr@gmail.com>
2022-07-06 17:47:53 +0300

    Web Development Ok

--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -13,6 +13,36 @@ namespace NLayer.Repository
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures { get; set; }
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if (item.Entity is BaseEntity entityReference)
+                {
+                    switch (item.State)
+                    {
+                        case EntityState.Added: { entityReference.CreateDate=DateTime.Now; break; }
+                            case EntityState.Modified: { entityReference.UpdateDate=DateTime.Now; break; }
+                    }
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+        public override int SaveChanges()
+        {
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if (item.Entity is BaseEntity entityReference)
+                {
+                    switch (item.State)
+                    {
+                        case EntityState.Added: { entityReference.CreateDate = DateTime.Now; break; }
+                        case EntityState.Modified: { Entry(entityReference).Property(x => x.CreateDate).IsModified = false; entityReference.UpdateDate = DateTime.Now; break; }
+                    }
+                }
+            }
+            return base.SaveChanges();
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<ProductFeature>().HasData(new ProductFeature()
