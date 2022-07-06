8d196dbadfc39a70440accdaaace9aa4c87109c7
Muhammed <bymadmr@gmail.com>
2022-07-06 20:28:08 +0300

    Project Completed

--- NLayer.API/Controllers/CategoriesController.cs
+++ NLayer.API/Controllers/CategoriesController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
 using NLayer.Core.Services;
 
 namespace NLayer.API.Controllers
@@ -8,9 +10,18 @@ namespace NLayer.API.Controllers
     public class CategoriesController : CustomBaseController
     {
         private readonly ICategoryService _categoryService;
-        public CategoriesController(ICategoryService categoryService)
+        private readonly IMapper _mappper;
+        public CategoriesController(ICategoryService categoryService, IMapper mappper)
         {
             _categoryService = categoryService;
+            _mappper = mappper;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            var categortyDto = _mappper.Map<List<CategoryDto>>(categories.ToList());
+            return CreateActionResult(CustomResponseDto<List<CategoryDto>>.Success(200, categortyDto));
         }
 
         [HttpGet("[action]/{categoryId}")]
